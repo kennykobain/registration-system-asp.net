08d4ffb12a5b1d22bc39e81f346ea0f4afe1f165
kennykobain <kenenduba@gmail.com>
2023-05-18 14:52:04 -0400

    initial commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lab1.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+    [BindProperty]
+    public int Number1 { get; set; }
+
+    
+
+    public void OnPost()
+    {
+        Console.WriteLine("This is post");
+        Console.WriteLine(Number1);
+    }
+}
